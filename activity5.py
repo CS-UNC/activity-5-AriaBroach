bb80ae19b844befa824dc3a6fb93dbd7f9dc9c2c
AriaBroach <broa8195@bears.unco.edu>
2025-10-22 23:53:07 +0000

    Finishing Assignment

--- activity5.py
+++ activity5.py
@@ -44,4 +44,51 @@ print(uses_only('abracadabra','abr'))
 
 
 def all_uses_only(file,letters):
-    open(file,'r')
+    result = []
+    with open(file,'r') as file:
+        for line in file:
+            words= line.split()
+            for word in words:
+                if uses_only(word,letters):
+                    result.append(word)
+    
+    return result
+
+print(all_uses_only('CROSSWD.txt','arc'))
+
+
+# grades= []
+# Names = ['Alex','Max','Carey']
+# grades.append([10,9,8,7])
+# grades.append([9,9,8,8])
+# grades.append([10,10,10,2])
+
+# #print(grades)
+# x = Names.index('Carey')
+# print(grades[x][3])
+
+# grades = {}
+# grades['Max'] = {'A1':10,'A2': 9,'A3': 8,'A4': 7}
+# grades['Alex'] = [9,9,8,8]
+# grades['Carey'] = [10,10,10,2]
+
+# print(grades.keys())
+# for student in grades.keys():
+#     print(student, grades[student])
+
+# print(grades['Max']['A2'])
+
+# statement = "This is not cool"
+# data = [12,13,14,15,16]
+# print(statement)
+# print(data)
+# print('--------------------')
+
+# data.append(35)
+# print(data)
+# statement= statement + ' modified'
+# print(statement)
+
+# statement2=statement.upper()
+# print(statement2)
+# print(statement is statement2)
